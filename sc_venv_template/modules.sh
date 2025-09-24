b34f7e83f8ad22c6deabd5e7c2aab1aa9ff0917a
Benjamin Meyjohann <benjamin.meyjohann@gmx.de>
2025-09-24 10:40:42 +0200

    Revert changes for isaacsim.

--- sc_venv_template/modules.sh
+++ sc_venv_template/modules.sh
@@ -1,12 +1,11 @@
 module purge
-module load Stages/2024
+module load Stages/2025
 module load GCC OpenMPI
-module load Python/3.11.3
 # Some base modules commonly used in AI
 module load mpi4py numba tqdm matplotlib IPython SciPy-Stack bokeh git
 module load Flask Seaborn
 
 # ML Frameworks
-module load  PyTorch scikit-learn torchvision PyTorch-Lightning
+module load PyTorch scikit-learn torchvision PyTorch-Lightning
 module load tensorboard
 module load h5py
