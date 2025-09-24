ce2b458e3907428cb0d1e2c49d7f71b81c4cdd2e
Benjamin Meyjohann <benjamin.meyjohann@gmx.de>
2025-09-24 10:28:05 +0200

    Cluster env adjustment for isaacsim.

--- sc_venv_template/modules.sh
+++ sc_venv_template/modules.sh
@@ -1,6 +1,7 @@
 module purge
-module load Stages/2025
+module load Stages/2024
 module load GCC OpenMPI
+module load Python/3.11.3
 # Some base modules commonly used in AI
 module load mpi4py numba tqdm matplotlib IPython SciPy-Stack bokeh git
 module load Flask Seaborn
