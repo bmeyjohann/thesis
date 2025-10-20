6f4752feba3615b905e5db69f99211456e5d113c
Benjamin Meyjohann <benjamin.meyjohann@gmx.de>
2025-10-20 12:01:28 +0200

    Fix env?

--- sc_venv_template/modules.sh
+++ sc_venv_template/modules.sh
@@ -6,6 +6,6 @@ module load mpi4py numba tqdm matplotlib IPython SciPy-Stack bokeh git
 module load Flask Seaborn
 
 # ML Frameworks
-module load PyTorch scikit-learn torchvision PyTorch-Lightning
+module load PyTorch scikit-learn torchvision PyTorch-Lightning cuDNN/9
 module load tensorboard
 module load h5py
